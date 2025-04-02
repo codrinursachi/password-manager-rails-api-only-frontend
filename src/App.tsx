afd951fd6bbd5df7e44a232e6816a67330f8de72
codrinursachi <codrinursachi@gmail.com>
2025-04-02 21:19:22 +0300

    add shadcn sidebar, add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
-import { StrictMode } from "react";
+import LoginsPage from "./pages/LoginsPage";
+import { useState } from "react";
+import RootLayout from "./pages/RootLayout";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return (
-    <StrictMode>
-      <LoginPage />
-    </StrictMode>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/", element: <LoginsPage isLoggedIn={isLoggedIn} /> },
+        {path: "/logins", element: <LoginsPage isLoggedIn={isLoggedIn} />},
+      ],
+    },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/register", element: <RegisterPage /> },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/RootLayout.tsx
@@ -0,0 +1,17 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { Outlet } from "react-router";
+
+function RootLayout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <Outlet />
+      </main>
+    </SidebarProvider>
+  );
+}
+
+export default RootLayout;
